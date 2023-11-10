64fc226e5d3a4b62c9b1a17c71cbda4af6640a84
dseveria <dseveria@redhat.com>
2023-11-10 16:23:04 +0100

    configure parameters

--- scripts/createAll.sh
+++ scripts/createAll.sh
@@ -2,8 +2,6 @@ oc apply -f files/namespaces-for-workloads.yaml
 oc apply -f files/namespaces-for-argocd-instance.yaml
 ./scripts/createUsersCRC.sh
 oc apply -f files/gitops-group-users.yaml
-oc apply -f files/argocd-operatorgroup.yaml
-oc apply -f files/argocd-controller-role-exclusions.yaml
 oc apply -f files/openshift-gitops-subscription.yaml
 sleep 3m
 oc apply -f files/argocd-instance-namespace-scope.yaml
